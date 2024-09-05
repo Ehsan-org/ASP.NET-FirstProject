93fb6f6d309f296e8320a0e862c36f9f8faab801
Ehsan Nadernezhad <135884292+ehsan-nadernezhad@users.noreply.github.com>
2024-09-05 23:09:27 +0200

    Delete EhsanWeb directory
    Delete unnecessary Data.

--- EhsanWeb/EhsanWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EhsanWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
