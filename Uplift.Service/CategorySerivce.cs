c47dc912f6f65f67b693661b5aa71d7d69c05938
Ehsan Nadernezhad <135884292+ehsan-nadernezhad@users.noreply.github.com>
2024-10-08 21:11:19 +0200

    Update CategorySerivce.cs
    I change codes. I use UnitOfWork on this code.

--- Uplift.Service/CategorySerivce.cs
+++ Uplift.Service/CategorySerivce.cs
@@ -1,87 +1,26 @@
-﻿//using Uplift.DataAccess.Data.Repository.IRepository;
-//using Uplift.Models;
-
-//namespace Uplift.Service
-//{
-//    public class CategoryService : ICategoryService
-//    {
-//        private readonly IUnitOfWork _unitOfWork;
-
-//        public CategoryService(IUnitOfWork unitOfWork) 
-//        {
-//            _unitOfWork = unitOfWork;
-//        }
-
-//        public void Create(Category model)
-//        {
-//            _unitOfWork.Category.Add(model);
-//            _unitOfWork.Save();
-//        }
-//        public IEnumerable<Category> GetAllCategories()
-//        {
-//            return _unitOfWork.Category.GetAll();
-//        }
-//    }
-//}
-
-using Microsoft.EntityFrameworkCore;
-using Uplift.DataAccess.Data;
+using Uplift.DataAccess.Data.Repository.IRepository;
 using Uplift.Models;
 
 namespace Uplift.Service
 {
     public class CategoryService : ICategoryService
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CategoryService(ApplicationDbContext context)
+        public CategoryService(IUnitOfWork unitOfWork) 
         {
-            _context = context;
+            _unitOfWork = unitOfWork;
         }
 
-        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        public void Create(Category model)
         {
-            return await _context.Categories.ToListAsync();
+            _unitOfWork.Category.Add(model);
+            _unitOfWork.Save();
         }
-
-        public async Task<Category> GetCategoryByIdAsync(int id)
+        public IEnumerable<Category> GetAllCategories()
         {
-            return await _context.Categories.FindAsync(id);
-        }
-
-        public async Task CreateCategoryAsync(Category category)
-        {
-            await _context.Categories.AddAsync(category);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<bool> UpdateCategoryAsync(Category category)
-        {
-            var existingCategory = await _context.Categories.FindAsync(category.Id);
-            if (existingCategory == null)
-            {
-                return false;
-            }
-
-            existingCategory.Name = category.Name;
-            existingCategory.DisplayOrder = category.DisplayOrder;
-
-            _context.Categories.Update(existingCategory);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<bool> DeleteCategoryAsync(int id)
-        {
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return false;
-            }
-
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
-            return true;
+            return _unitOfWork.Category.GetAll();
         }
     }
 }
+
